84246e3fdbc1e5f517515d2c332164e20625c88e
Rodrigo Alejandro Melo <rodrigomelo9@gmail.com>
2021-02-20 11:44:48 -0300

    libero: updated to use the resources submodule

--- libero/flow.tcl
+++ libero/flow.tcl
@@ -2,13 +2,27 @@ new_project -name example -location libero -hdl {VHDL} -family {SmartFusion2}
 
 set_device -family SmartFusion2 -die M2S010 -package tq144 -speed -1
 
-create_links -hdl_source ../../hdl/blinking.vhdl
-create_links -io_pdc ../../examples/libero/mkr.pdc
+create_links -hdl_source ../resources/vhdl/blink.vhdl
+create_links -sdc ../resources/constraints/maker-board/clk.sdc
+create_links -io_pdc ../resources/constraints/maker-board/clk.pdc
+create_links -io_pdc ../resources/constraints/maker-board/led.pdc
 build_design_hierarchy
 
-set_root Blinking
+set_root Blink
 
-organize_tool_files -tool {PLACEROUTE} -file ../../examples/libero/mkr.pdc -module Blinking -input_type {constraint}
+organize_tool_files -tool {SYNTHESIZE} \
+  -file ../resources/constraints/maker-board/clk.sdc \
+  -module Blink -input_type {constraint}
+
+organize_tool_files -tool {PLACEROUTE} \
+  -file ../resources/constraints/maker-board/clk.sdc \
+  -file ../resources/constraints/maker-board/clk.pdc \
+  -file ../resources/constraints/maker-board/led.pdc \
+  -module Blink -input_type {constraint}
+
+organize_tool_files -tool {VERIFYTIMING} \
+  -file ../resources/constraints/maker-board/clk.sdc \
+  -module Blink -input_type {constraint}
 
 run_tool -name {SYNTHESIZE}
 
