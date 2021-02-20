ffd43373a6a33d78d7f22989b0020a2994adf7db
Rodrigo Alejandro Melo <rodrigomelo9@gmail.com>
2021-02-20 11:21:02 -0300

    vivado: updated to use the resources submodule

--- vivado/params.tcl
+++ vivado/params.tcl
@@ -6,7 +6,7 @@ set_property "part" xc7z010-1-clg400 [current_project]
 # you must specify the value using the Verilog bit format, rather than
 # standard VHDL format.
 
-add_files ../../hdl/fakes/parameters.v
+add_files ../resources/verilog/parameters.v
 set_property top Params [current_fileset]
 set_property "generic" "BOO=1 INT=255 LOG=1'b1 VEC=8'b11111111 STR=WXYZ REA=1.1" -objects [get_filesets sources_1]
 
@@ -17,7 +17,7 @@ wait_on_run synth_1
 # To avoid re-synthesis of the Verilog version
 remove_files [get_files]
 
-add_files ../../hdl/fakes/generics.vhdl
+add_files ../resources/vhdl/generics.vhdl
 set_property top Params [current_fileset]
 # TODO: add REA=1.1 (possible?)
 set_property "generic" "BOO=true INT=255 LOG=1'b1 VEC=8'b11111111 STR=WXYZ" -objects [get_filesets sources_1]
