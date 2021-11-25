5a97fdcffb49508cbec4e97b447c6b223a731be4
Rodrigo A. Melo <rodrigomelo9@gmail.com>
2021-11-25 20:21:15 -0300

    Added some checks into .settings.sh

--- .settings.sh
+++ .settings.sh
@@ -1,3 +1,5 @@
+TOOLS="vivado ise quartus libero stop stat"
+
 # PATHs
 
 ISE_DIR=/opt/Xilinx/ise
@@ -18,49 +20,77 @@ LIBERO_LIC_LOG=/tmp/libero-license.log
 TOOL=$1
 
 if [ -z "$TOOL" ]; then
-    TOOL=all
+   TOOL=all
+else
+   if [[ ! $TOOLS == *$TOOL* ]]; then
+      echo "Unsuported tool $TOOL";
+   fi
 fi
 
 ## Xilinx
 
 if [ $TOOL == "all" ] || [ $TOOL == "vivado" ]; then
-    echo "Configuring Vivado"
-    export PATH=$VIVADO_DIR/bin:$PATH
+   echo "Configuring Vivado"
+   if [[ -d $VIVADO_DIR ]]; then
+      export PATH=$VIVADO_DIR/bin:$PATH
+   else
+      echo "$VIVADO_DIR doesn't exists"
+   fi
 fi
 
 if [ $TOOL == "all" ] || [ $TOOL == "ise" ]; then
-    echo "Configuring ISE"
-    export PATH=$ISE_DIR/ISE/bin/lin64:$PATH
+   echo "Configuring ISE"
+   if [[ -d $ISE_DIR ]]; then
+      export PATH=$ISE_DIR/ISE/bin/lin64:$PATH
+   else
+      echo "$ISE_DIR doesn't exists"
+   fi
 fi
 
 ## Intel/Altera
 
 if [ $TOOL == "all" ] || [ $TOOL == "quartus" ]; then
-    echo "Configuring Quartus"
-    export PATH=$PATH:$QUARTUS_DIR/quartus/bin
-    # Platform Designer (Qsys) workaround
-    export PERL5LIB=$QUARTUS_DIR/quartus/linux64/perl/lib/5.28.1
+   echo "Configuring Quartus"
+   if [[ -d $QUARTUS_DIR ]]; then
+      export PATH=$PATH:$QUARTUS_DIR/quartus/bin
+      # Platform Designer (Qsys) workaround
+      export PERL5LIB=$QUARTUS_DIR/quartus/linux64/perl/lib/5.28.1
+   else
+      echo "$QUARTUS_DIR doesn't exists"
+   fi
 fi
 
 # Microchip/Microsemi/Actel
 
 if [ $TOOL == "all" ] || [ $TOOL == "libero" ]; then
-    echo "Configuring Libero"
-    export PATH=$PATH:$LIBERO_DIR/Libero/bin
-    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/x86_64-linux-gnu:/lib/x86_64-linux-gnu
-    export LM_LICENSE_FILE=$LIBERO_LIC_PORT@$LIBERO_LIC_HOST
-    if [ -z `pidof lmgrd` ]; then
-        echo "Launching Microsemi License manager... "
-        $LIBERO_LMGRD_DIR/lmgrd -c $LIBERO_LIC_FILE -l $LIBERO_LIC_LOG
-    else
-        echo "Microsemi License manager is already running... "
-    fi
+   echo "Configuring Libero"
+   if [[ -d $LIBERO_DIR ]]; then
+      export PATH=$PATH:$LIBERO_DIR/Libero/bin
+      export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/x86_64-linux-gnu:/lib/x86_64-linux-gnu
+      export LM_LICENSE_FILE=$LIBERO_LIC_PORT@$LIBERO_LIC_HOST
+      if [ -z `pidof lmgrd` ]; then
+         echo "Launching Microsemi License manager... "
+         $LIBERO_LMGRD_DIR/lmgrd -c $LIBERO_LIC_FILE -l $LIBERO_LIC_LOG
+      else
+         echo "Microsemi License manager is already running... "
+      fi
+   else
+      echo "$LIBERO_DIR doesn't exists"
+   fi
 fi
 
 if [ $TOOL == "stop" ]; then
-    $LIBERO_LMGRD_DIR/lmutil lmdown -c $LIBERO_LIC_FILE -q
+   if [[ -d $LIBERO_LMGRD_DIR ]]; then
+      $LIBERO_LMGRD_DIR/lmutil lmdown -c $LIBERO_LIC_FILE -q
+   else
+      echo "$LIBERO_LMGRD_DIR doesn't exists"
+   fi
 fi
 
 if [ $TOOL == "stat" ]; then
-    $LIBERO_LMGRD_DIR/lmutil lmstat -c $LIBERO_LIC_FILE
+   if [[ -d $LIBERO_LMGRD_DIR ]]; then
+      $LIBERO_LMGRD_DIR/lmutil lmstat -c $LIBERO_LIC_FILE
+   else
+      echo "$LIBERO_LMGRD_DIR doesn't exists"
+   fi
 fi
