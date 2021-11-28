82a7744bedbbd30b7f59f7e5d04c21438f218961
Rodrigo A. Melo <rodrigomelo9@gmail.com>
2021-11-28 14:03:24 -0300

    ghdl: added the check of a CHARACTER generic and a README.md file with notes

---
+++ ghdl/README.md
@@ -0,0 +1,7 @@
+# Notes about GHDL
+
+> Last update: Nov 2021
+
+* Support to specify a REAL generic is not working
+  * unhandled override for generic "rea"
+  * As a workaround, I set SKIP_REA
--- ghdl/vhdl.sh
+++ ghdl/vhdl.sh
@@ -7,8 +7,6 @@ FLAGS="--std=08 -fsynopsys -fexplicit -frelaxed"
 ghdl -a $FLAGS --work=blink_lib ../resources/vhdl/blink.vhdl
 ghdl -a $FLAGS --work=blink_lib ../resources/vhdl/blink_pkg.vhdl
 ghdl -a $FLAGS ../resources/vhdl/top.vhdl
-ghdl --synth $FLAGS -gBOO=true -gINT=255 -gLOG=\'1\' -gSTR="WXYZ" -gVEC="11111111" Top GHDL
 
-## -gREA=1.1 -> unhandled override for generic "rea"
-
-
+GENERICS="-gBOO=true -gINT=255 -gLOG='1' -gVEC="11111111" -gCHR='Z' -gSTR="WXYZ" -gSKIP_REA=1"
+ghdl --synth $FLAGS $GENERICS Top ARCH_SEL
